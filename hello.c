cc9e8026ecf5831420fb0a646356bc979c33e6aa
Mahan <mkhafajizad3@huskers.unl.edu>
2021-08-26 14:10:31 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: mahan Khafaji Zad
+ *
+ * Date: 2020/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char **argv) {
+
+  printf("Mahan Khafaji Zad\n");
+
+  return 0;
+}
